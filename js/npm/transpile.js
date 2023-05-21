c8ab4335927ac2b746c3b09f03007890a53a3649
Dan Allen <dan@opendevise.com>
2023-05-20 23:58:28 -0600

    use npx to manage installation of opal-compiler for build task

--- js/npm/transpile.js
+++ js/npm/transpile.js
@@ -1,15 +1,25 @@
 'use strict'
 
-const { Builder } = require('opal-compiler')
+const { env: ENV } = require('node:process')
 const fs = require('node:fs')
+const ospath = require('node:path')
 
-const transpiled = Builder
+let opalCompilerPath = 'opal-compiler'
+try {
+  require.resolve(opalCompilerPath)
+} catch {
+  const npxInstallDir = ENV.PATH.split(':')[0]
+  if (npxInstallDir?.endsWith('/node_modules/.bin') && npxInstallDir.startsWith(ENV.npm_config_cache + '/')) {
+    opalCompilerPath = require.resolve('opal-compiler', { paths: [ospath.dirname(npxInstallDir)] })
+  }
+}
+
+const transpiled = require(opalCompilerPath).Builder
   .create()
   .build('../lib/asciidoctor/tabs/block.rb')
   .build('../lib/asciidoctor/tabs/docinfo.rb')
   .build('../lib/asciidoctor/tabs/extensions.rb')
   .toString()
-
 fs.mkdirSync('dist', { recursive: true })
 fs.writeFileSync('dist/index.js', transpiled)
 fs.cpSync('../data', 'dist', { recursive: true })
